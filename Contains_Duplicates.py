d297213ff0b0066e313f7a3056d6f188a353354e
Stellah Rotich <37912118+stellahrotich@users.noreply.github.com>
2022-05-22 11:30:22 +0300

    Added use of hashset
    O(n) time, O(n) space , Set allows us to add elements in O(1) time

--- Contains_Duplicates.py
+++ Contains_Duplicates.py
@@ -1,3 +1,12 @@
+class Solution:
+    def containsDuplicate(self, nums):
+        hashset = set()
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+    
 class Solution(object):
     """Given an integer array nums, return true if any value appears at least twice in the array,
     and return false if every element is distinct."""
